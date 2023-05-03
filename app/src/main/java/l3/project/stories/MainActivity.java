0045d7aa4f6c1cc5e30bb36af23b49081edf2ace
khoubziabdelhak <khoubziabdelhak03@gmail.com>
2023-05-03 10:44:26 +0100

    good start

--- app/src/main/java/l3/project/stories/MainActivity.java
+++ app/src/main/java/l3/project/stories/MainActivity.java
@@ -10,11 +10,11 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    Story[] stories = { new Story("Story 1", "Description 1", R.drawable.storyimage1, new Scene[]{
-                    new Scene("Scene 1", "this is just a text context of the FIRST story scene", R.drawable.storyimage1),
-                    new Scene("Scene 1", "this is just a text context of a story scene", R.drawable.storyimage1),
-                    new Scene("Scene 1", "this is just a text context of a story scene", R.drawable.storyimage1),
-            }),
+    Story[] stories = {new Story("Story 1", "Description 1", R.drawable.storyimage1, new Scene[]{
+            new Scene("Scene 1", "This is a scene of the kid story that i should be a little tall so i can test if the thing is working i am just typing to make it a little long, so don\'t mind ma and keep working on your project i guess you are doing great i need to make it a little longer...", R.drawable.storyimage1),
+            new Scene("Scene 1", "this is just a text context of a story scene", R.drawable.storyimage1),
+            new Scene("Scene 1", "this is just a text context of a story scene", R.drawable.storyimage1),
+    }),
             new Story("Story 2", "Description 2", R.drawable.storyimage2, new Scene[]{
                     new Scene("Scene 1", "this is just a text context of the SECOND story scene", R.drawable.storyimage2),
                     new Scene("Scene 1", "this is just a text context of a story scene", R.drawable.storyimage1),
